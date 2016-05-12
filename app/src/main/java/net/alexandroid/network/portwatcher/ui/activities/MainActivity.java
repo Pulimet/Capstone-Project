099597b618ee8c202b4a783b0994fbb2e12b8c72
Alexey <korolev2009@gmail.com>
2016-05-12 15:00:02 +0300

    Create empty fragments for all conceived items

--- app/src/main/java/net/alexandroid/network/portwatcher/ui/activities/MainActivity.java
+++ app/src/main/java/net/alexandroid/network/portwatcher/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package net.alexandroid.network.portwatcher.ui.activities;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,7 +17,11 @@ import android.view.View;
 import net.alexandroid.network.portwatcher.R;
 import net.alexandroid.network.portwatcher.helpers.MyLog;
 import net.alexandroid.network.portwatcher.objects.ScanItem;
+import net.alexandroid.network.portwatcher.ui.fragments.EditFragment;
 import net.alexandroid.network.portwatcher.ui.fragments.MainHistoryFragment;
+import net.alexandroid.network.portwatcher.ui.fragments.ScanFragment;
+import net.alexandroid.network.portwatcher.ui.fragments.ScheduleFragment;
+import net.alexandroid.network.portwatcher.ui.fragments.WatchFragment;
 
 @SuppressWarnings("ConstantConditions")
 public class MainActivity extends AppCompatActivity implements
@@ -25,16 +30,21 @@ public class MainActivity extends AppCompatActivity implements
         MainHistoryFragment.OnListOfMainFragmentInteractionListener {
 
 
-    public static final int FRAGMENT_MAIN = 0;
+    public static final int FRAGMENT_MAIN_HISTORY = 0;
+    public static final int FRAGMENT_SCAN = 1;
+    public static final int FRAGMENT_EDIT = 2;
+    public static final int FRAGMENT_WATCH = 3;
+    public static final int FRAGMENT_SCHEDULE = 4;
 
-    private  SearchView mSearchView;
+    private SearchView mSearchView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        seToolBArAndNavigation();
+        seToolBarAndNavigation();
         setFab();
         showMainFragment(savedInstanceState);
     }
@@ -106,18 +116,30 @@ public class MainActivity extends AppCompatActivity implements
     public boolean onNavigationItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_history:
+                replaceFragmentWith(FRAGMENT_MAIN_HISTORY, false);
+                mToolbar.setSubtitle(R.string.history);
                 break;
             case R.id.nav_scan:
+                replaceFragmentWith(FRAGMENT_SCAN, false);
+                mToolbar.setSubtitle(R.string.scan);
                 break;
             case R.id.nav_edit:
+                replaceFragmentWith(FRAGMENT_EDIT, false);
+                mToolbar.setSubtitle(R.string.edit);
                 break;
             case R.id.nav_watchlist:
+                replaceFragmentWith(FRAGMENT_WATCH, false);
+                mToolbar.setSubtitle(R.string.watchlist);
                 break;
             case R.id.nav_schedule:
+                replaceFragmentWith(FRAGMENT_SCHEDULE, false);
+                mToolbar.setSubtitle(R.string.schedule);
                 break;
             case R.id.nav_share:
+                // TODO share action
                 break;
             case R.id.nav_rate:
+                // TODO rate action
                 break;
         }
 
@@ -129,26 +151,62 @@ public class MainActivity extends AppCompatActivity implements
         return true;
     }
 
-    private void seToolBArAndNavigation() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private void seToolBarAndNavigation() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.getMenu().getItem(0).setChecked(true);
 
+        navigationView.getMenu().getItem(0).setChecked(true);
+        mToolbar.setSubtitle(R.string.history);
     }
 
     private void showMainFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, new MainHistoryFragment(), "tag" + FRAGMENT_MAIN).commit();
+                    .add(R.id.fragment_container, new MainHistoryFragment(), "tag" + FRAGMENT_MAIN_HISTORY).commit();
+        }
+    }
+
+    private void replaceFragmentWith(int fragmentInt, boolean addToBackStack) {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("tag" + fragmentInt);
+        if (fragment == null) {
+            MyLog.d("replaceFragmentWith - fragment == null");
+            switch (fragmentInt) {
+                case FRAGMENT_MAIN_HISTORY:
+                    fragment = new MainHistoryFragment();
+                    break;
+                case FRAGMENT_SCAN:
+                    fragment = new ScanFragment();
+                    break;
+                case FRAGMENT_EDIT:
+                    fragment = new EditFragment();
+                    break;
+                case FRAGMENT_WATCH:
+                    fragment = new WatchFragment();
+                    break;
+                case FRAGMENT_SCHEDULE:
+                    fragment = new ScheduleFragment();
+                    break;
+                default:
+                    fragment = new MainHistoryFragment();
+                    break;
+            }
+        }
+
+        if (addToBackStack) {
+            getSupportFragmentManager().beginTransaction().addToBackStack(null)
+                    .replace(R.id.fragment_container, fragment, "tag" + fragmentInt).commitAllowingStateLoss();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment, "tag" + fragmentInt).commitAllowingStateLoss();
         }
     }
 
--- app/src/main/java/net/alexandroid/network/portwatcher/ui/fragments/ScheduleFragment.java
+++ app/src/main/java/net/alexandroid/network/portwatcher/ui/fragments/ScheduleFragment.java
@@ -1,55 +1,41 @@
 package net.alexandroid.network.portwatcher.ui.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.alexandroid.network.portwatcher.R;
-import net.alexandroid.network.portwatcher.objects.ScanItem;
-import net.alexandroid.network.portwatcher.ui.adapters.MainRecyclerAdapter;
-import net.alexandroid.network.portwatcher.ui.adapters.decorators.SimpleDividerItemDecoration;
 
 
 /**
  * A fragment representing a list of Items.
  * <p/>
- * Activities containing this fragment MUST implement the {@link OnListOfMainFragmentInteractionListener}
+ * Activities containing this fragment MUST implement the {@lin OnListOfMainFragmentInteractionListener}
  * interface.
  */
-public class MainHistoryFragment extends Fragment {
+public class ScheduleFragment extends Fragment {
 
-    private OnListOfMainFragmentInteractionListener mListener;
+    //private OnListOfMainFragmentInteractionListener mListener;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public MainHistoryFragment() {
+    public ScheduleFragment() {
     }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.addItemDecoration(new SimpleDividerItemDecoration(getContext().getApplicationContext()));
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            //recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            recyclerView.setAdapter(new MainRecyclerAdapter(ScanItem.getDummyList(), mListener));
-        }
         return view;
     }
 
-
+/*
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -67,17 +53,13 @@ public class MainHistoryFragment extends Fragment {
         mListener = null;
     }
 
-    /**
+    *//**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     */
+     *//*
     public interface OnListOfMainFragmentInteractionListener {
         void onItemClick(ScanItem item);
-
-        void onStarClick(ScanItem item);
-
-        void onShareClick(ScanItem item);
-    }
+    }*/
 }
