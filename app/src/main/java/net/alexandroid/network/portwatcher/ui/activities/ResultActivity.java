4b22de3d27ba93a57fce8520c90ac8c081b8efcb
Alexey <korolev2009@gmail.com>
2016-05-27 14:26:27 +0300

    ResultActivity UI 2

--- app/src/main/java/net/alexandroid/network/portwatcher/ui/activities/ResultActivity.java
+++ app/src/main/java/net/alexandroid/network/portwatcher/ui/activities/ResultActivity.java
@@ -1,15 +1,36 @@
 package net.alexandroid.network.portwatcher.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import net.alexandroid.network.portwatcher.R;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        setClickListener();
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setClickListener() {
+        findViewById(R.id.btnRescan).setOnClickListener(this);
+        findViewById(R.id.btnOk).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnOk:
+                finish();
+                break;
+            case R.id.btnRescan:
+                // TODO: Rescan
+                break;
+        }
     }
 }
