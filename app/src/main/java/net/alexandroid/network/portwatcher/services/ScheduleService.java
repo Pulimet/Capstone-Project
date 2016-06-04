463e667fd08553f8d2ed8e7d1583af3d5fb9cfff
Alexey <korolev2009@gmail.com>
2016-06-04 17:58:49 +0300

    Schedule control (MainActivity) 3

--- app/src/main/java/net/alexandroid/network/portwatcher/services/ScheduleService.java
+++ app/src/main/java/net/alexandroid/network/portwatcher/services/ScheduleService.java
@@ -1,14 +1,20 @@
 package net.alexandroid.network.portwatcher.services;
 
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
+import com.google.android.gms.gcm.PeriodicTask;
 import com.google.android.gms.gcm.TaskParams;
 
+import net.alexandroid.network.portwatcher.data.DbContract;
 import net.alexandroid.network.portwatcher.helpers.MyLog;
 import net.alexandroid.network.portwatcher.helpers.Utils;
+import net.alexandroid.network.portwatcher.ui.fragments.ScheduleFragment;
 
 public class ScheduleService extends GcmTaskService {
 
@@ -22,8 +28,39 @@ public class ScheduleService extends GcmTaskService {
         // an updated package. This is not called when your application is first installed.
         //
         // This is called on your application's main thread.
-
         // TODO(developer): In a real app, this should be implemented to re-schedule important tasks.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ContentResolver cr = getContentResolver();
+                Cursor c = cr.query(DbContract.ScheduleEntry.CONTENT_URI, null, null, null, null);
+                if (c == null) {
+                    return;
+                }
+                while (c.moveToNext()) {
+                    String host = c.getString(ScheduleFragment.COL_HOST);
+                    String ports = c.getString(ScheduleFragment.COL_PORTS);
+                    String interval = c.getString(ScheduleFragment.COL_INTERVAL);
+
+                    MyLog.d("addSchedule, host: " + host + "  ports: " + ports + "   Interval: " + interval);
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString(ScheduleService.EXTRA_HOST, host);
+                    bundle.putString(ScheduleService.EXTRA_PORT, ports);
+
+                    PeriodicTask task = new PeriodicTask.Builder()
+                            .setService(ScheduleService.class)
+                            .setExtras(bundle)
+                            .setTag(Utils.createAlarmTag(host, ports, interval))
+                            .setPeriod(Long.valueOf(interval) / 1000)
+                            .build();
+
+                    GcmNetworkManager.getInstance(getApplicationContext()).schedule(task);
+                }
+                c.close();
+            }
+        }).start();
+
     }
 
 
@@ -38,13 +75,15 @@ public class ScheduleService extends GcmTaskService {
         String ports = bundle.getString(EXTRA_PORT);
 
         MyLog.d("host: " + host + "   ports: " + ports + "   onRunTask: " + tag);
-
-        Intent intent = new Intent(ScheduleService.this, ScanService.class);
-        intent.putExtra(ScanService.EXTRA_HOST, host);
-        intent.putExtra(ScanService.EXTRA_SCAN_ID, -1);
-        intent.putIntegerArrayListExtra(ScanService.EXTRA_PORTS, Utils.convertStringToIntegerList(ports));
-        startService(intent);
-
+        startScanService(host, ports);
         return 0;
+    }
+
+    private void startScanService(String pHost, String pPorts) {
+        Intent intent = new Intent(ScheduleService.this, ScanService.class);
+        intent.putExtra(ScanService.EXTRA_HOST, pHost);
+        intent.putExtra(ScanService.EXTRA_SCAN_ID, -1);
+        intent.putIntegerArrayListExtra(ScanService.EXTRA_PORTS, Utils.convertStringToIntegerList(pPorts));
+        startService(intent);
     }
 }
